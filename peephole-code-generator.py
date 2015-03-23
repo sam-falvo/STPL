a8fcdce66e3301fdbe77acb2a18d1c23cb5b675b
Samuel A. Falvo II <kc5tja@arrl.net>
2015-03-23 00:09:30 -0700

    Support basic blocks, subroutines, returns, if/then

--- peephole-code-generator.py
+++ peephole-code-generator.py
@@ -6,9 +6,52 @@ class Insn(object):
         return self.opc == "ori" and self.dest != 0 and self.src1 == 0
 
 
+label_counter = 0
+
+def new_label_name():
+    global label_counter
+    label_counter = label_counter + 1
+    return "L{}".format(label_counter)
+
+
+class BEQ(Insn):
+    def __init__(self, r1, r2, dest):
+        self.opc = "beq"
+        self.src1 = r1
+        self.src2 = r2
+        self.dest = dest
+
+    def __repr__(self):
+        return "\tbeq\tX{}, X{}, {}".format(self.src1, self.src2, self.dest.name)
+
+
+class Label(Insn):
+    def __init__(self, name=None):
+        self.opc = ":label:"
+
+        if not name:
+            self.name = new_label_name()
+        else:
+            self.name = name
+
+    def __repr__(self):
+        return "\n{}:".format(self.name)
+
+
 class ADD(Insn):
     def __init__(self, dest, src1, src2):
         self.opc = "add"
+        self.dest = dest
+        self.src1 = src1
+        self.src2 = src2
+
+    def __repr__(self):
+        return "\t{}\tX{}, X{}, X{}".format(self.opc, self.dest, self.src1, self.src2)
+
+
+class XOR(Insn):
+    def __init__(self, dest, src1, src2):
+        self.opc = "xor"
         self.dest = dest
         self.src1 = src1
         self.src2 = src2
@@ -68,6 +111,27 @@ class SD(Insn):
     def __repr__(self):
         return "\t{}\tX{}, {}(X{})".format(self.opc, self.src, self.offset, self.index)
 
+class JALR(Insn):
+    def __init__(self, dest, offset, index):
+        self.opc = "jalr"
+        self.dest = dest
+        self.offset = offset
+        self.index = index
+
+    def __repr__(self):
+        return "\t{}\tX{}, {}(X{})".format(self.opc, self.dest, self.offset, self.index)
+
+
+class JAL(Insn):
+    def __init__(self, dest, label):
+        self.opc = "jal"
+        self.dest = dest
+        self.label = label
+
+    def __repr__(self):
+        return "\t{}\tX{}, {}".format(self.opc, self.dest, self.label.name)
+
+
 
 class Optimizer(object):
     RA = 1
@@ -90,12 +154,17 @@ class Optimizer(object):
     def reset(self):
         self.C = []
         self.I = []
-        self.regs_dstack = [self.DC]
         self.regs_rstack = []
-        self.dsp_offset = 0
         self.rsp_offset = 0
         self.regs_avail = [self.D0, self.D1, self.D2, self.D3, self.D4, self.D5, self.D6, self.D7]
         self.gp_base = None
+        self.reset_dstack()
+        self.ctrl = []
+        self.current_subroutine = None
+
+    def reset_dstack(self):
+        self.regs_dstack = [self.DC]
+        self.dsp_offset = 0
 
     def refill_register(self):
         if len(self.regs_avail) > 0:
@@ -127,6 +196,38 @@ class Optimizer(object):
         self.I.append(LD(r, self.dsp_offset, self.DSP))
         self.dsp_offset = self.dsp_offset + 8
 
+    def commit_stack(self):
+        if len(self.regs_dstack) < 2:
+            return
+
+        regs = self.regs_dstack[1:]
+        regs.reverse()
+        for r in regs:
+            self.dsp_offset = self.dsp_offset - 8
+            self.I.append(SD(r, self.dsp_offset, self.DSP))
+            if r != self.DC:
+                self.free_register(r)
+
+    def commit_stack_cache(self):
+        r = self.regs_dstack[0]
+        self.I.append(ORI(self.DC, r, 0))
+        self.free_register(r)
+
+    def commit(self):
+        self.commit_stack()
+
+        if (len(self.regs_dstack) >= 1) and (self.regs_dstack[0] != self.DC):
+            self.commit_stack_cache()
+        elif len(self.regs_dstack) == 0:
+            self.I.append(LD(self.DC, self.dsp_offset, self.DSP))
+            self.dsp_offset = self.dsp_offset + 8
+
+        if self.dsp_offset != 0:
+            self.I.append(ADDI(self.DSP, self.DSP, self.dsp_offset))
+            self.dsp_offset = 0
+
+        self.reset_dstack()
+
     def literal(self, n):
         if -2048 <= n < 2048:
             r = self.next_register()
@@ -138,6 +239,15 @@ class Optimizer(object):
                 self.gp_base = -4 * (len(self.I) + 2)
             offset = -8 * (len(self.C) - 1)
             self.I.append(LD(self.next_register(), self.gp_base + offset, self.GP))
+
+    def xor(self):
+        if len(self.regs_dstack) >= 2:
+            rd = self.regs_dstack[1]
+            rs = self.pop_register()
+            self.I.append(XOR(rd, rd, rs))
+        else:
+            self.bind_s()
+            self.xor()
 
     def add(self):
         if len(self.regs_dstack) >= 2:
@@ -213,11 +323,53 @@ class Optimizer(object):
             self.over()
 
     def drop(self):
-        self.pop_register()
+        if len(self.regs_dstack) >= 1:
+            self.pop_register()
+        else:
+            self.dsp_offset = self.dsp_offset + 8
+
+    def nip(self):
+        self.swap(); self.optimize()
+        self.drop(); self.optimize()
+
 
     def add_imm(self, imm):
         rd = self.regs_dstack[0]
         self.I.append(ADDI(rd, rd, imm))
+
+    def subroutine(self, name):
+        self.commit(); self.optimize()
+        l = Label(name)
+        self.I.append(l)
+        self.current_subroutine = l
+
+    def rfs(self):
+        self.commit(); self.optimize()
+        self.I.append(JALR(0, 0, self.RA))
+
+    def If(self):
+        if len(self.regs_dstack) < 1:
+            self.bind_s()
+            self.If()
+
+        r = self.pop_register()
+
+        self.commit(); self.optimize()
+        self.ctrl.insert(0, Label())
+        self.I.append(BEQ(r, 0, self.ctrl[0]))
+    
+    def Then(self):
+        self.commit(); self.optimize()
+        self.I.append(self.ctrl[0])
+        self.ctrl = self.ctrl[1:]
+
+    def call(self, name):
+        self.commit(); self.optimize()
+        self.I.append(JAL(self.RA, Label(name)))
+
+    def recurse(self):
+        self.commit(); self.optimize()
+        self.I.append(JAL(self.RA, self.current_subroutine))
 
     def optimize(self):
         while self.optimize_step():
@@ -252,68 +404,45 @@ class Optimizer(object):
                 self.I = self.I[:-2]
                 self.I.append(i)
                 return True
+            elif i1.opc == "jal" and i1.dest == self.RA and i0.opc == "jalr" and i0.dest == 0 and i0.offset == 0 and i0.index == self.RA:
+                i = JAL(0, i1.label)
+                self.I = self.I[:-2]
+                self.I.append(i)
+                return True
 
         return False
 
     def dump(self):
         for c in self.C:
             print("\tDD\t{}".format(c))
-        print("Entry:")
         for i in self.I:
             print(i)
-        print("-----")
+        print("\n")
 
 o = Optimizer()
-
-o.literal(4000)
-o.literal(5000)
-o.over()
-o.fetch()
-o.over()
-o.fetch()
-o.over()
-o.fetch()
-o.over()
-o.store()
-o.literal(80)
-o.add()
-o.swap()
-o.literal(256)
-o.add()
-o.push()
-o.swap()
-o.store()
-o.pop()
-o.swap()
-o.store()
-
-o.literal(3); o.optimize(); o.add(); o.optimize()
-o.dump()
-
-o.reset()
-
-# o.literal(4000); o.optimize()
-# o.literal(5000); o.optimize()
+o.subroutine("cls0")
+o.literal(65536); o.optimize()
 o.over(); o.optimize()
-o.fetch(); o.optimize()
-o.over(); o.optimize()
-o.fetch(); o.optimize()
-o.over(); o.optimize()
-o.fetch(); o.optimize()
+o.xor(); o.optimize()
+o.If(); o.optimize()
+o.literal(0); o.optimize()
 o.over(); o.optimize()
 o.store(); o.optimize()
-o.literal(80); o.optimize()
+o.literal(8); o.optimize()
 o.add(); o.optimize()
-o.swap(); o.optimize()
-o.literal(256); o.optimize()
-o.add(); o.optimize()
-o.push(); o.optimize()
-o.swap(); o.optimize()
-o.store(); o.optimize()
-o.pop(); o.optimize()
-o.swap(); o.optimize()
-o.store(); o.optimize()
+o.recurse(); o.optimize()
+o.rfs(); o.optimize()
+o.Then(); o.optimize()
+o.drop(); o.optimize()
+o.rfs(); o.optimize()
 
-o.literal(3); o.optimize(); o.add(); o.optimize()
-o.dump()
+o.commit(); o.dump()
+
+o = Optimizer()
+o.subroutine("cls")
+o.literal(49152); o.optimize()
+o.call("cls0"); o.optimize()
+o.rfs(); o.optimize()
+
+o.commit(); o.dump()
 
