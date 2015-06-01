7ceb89eb19ce91057dc19ee46703b3869e6732f0
Samuel A. Falvo II <sam.falvo@rackspace.com>
2015-06-01 11:27:19 -0700

    Scopes, inserts, query logic.

--- oil/symtab.py
+++ oil/symtab.py
@@ -1,5 +1,37 @@
+import attr
+
 from parser import Item
 
+
+@attr.s
 class SymTab(object):
+    scopes = attr.ib(default=None)
+
     def lookup(self, item, name):
+        if self.scopes is not None:
+            for scope in self.scopes:
+                if name in scope:
+                    j = scope[name]
+                    item.typ = j.typ
+                    item.cls = j.cls
+                    item.a = j.a
+                    return
         item.typ = Item.Unknown
+
+    def openScope(self):
+        if self.scopes is None:
+            self.scopes = list()
+        self.scopes.insert(0, {})
+
+    def closeScope(self):
+        if self.scopes is not None:
+            if len(self.scopes) > 0:
+                self.scopes = self.scopes[1:]
+
+    def insert(self, item, name):
+        if len(self.scopes) > 0:
+            scope = self.scopes[0]
+            scope[name] = item
+        else:
+            raise Exception("No scope available to insert into")
+
